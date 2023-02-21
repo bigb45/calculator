53c515f4c5b8c97858c1fe7c734314b346bb476d
bigb45 <bgd4500@gmail.com>
2023-02-21 17:12:58 +0300

    added operate function

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let operationButtons = document.querySelectorAll(".operation");
 let displayElement = document.querySelector(".display");
 let operand1 = 0;
 let operand2 = 0;
-let opeartion = "";
+let ans = 0;
+let hasOperand = false;
+let operation = "";
 let innerDisplayValue = 0;
 let displayValue = String(innerDisplayValue);
-
 
 operationButtons.forEach(function (button) {
   button.addEventListener(
@@ -20,17 +21,50 @@ numberButtons.forEach(function (button) {
 });
 
 function operationClicked(buttonClass) {
-  let operation = buttonClass[0];
-  if (operation != "clear") operand1 = innerDisplayValue;
-  console.log(operation);
-  innerDisplayValue = 0;
-  displayValue = 0;
+  if (buttonClass[0] != "equal") {
+    operation = buttonClass[0];
+  }
+  if (operation != "clear" && hasOperand) {
+    operand2 = innerDisplayValue;
+    hasOperand = false
+  } else {
+    operand1 = innerDisplayValue;
+    hasOperand = true;
+  }
+
+  console.log(operand1, operation, operand2);
+  if (buttonClass[0] == "equal") {
+    ans = operate(operation);
+  }
+
+  innerDisplayValue = displayValue = ans;
+  operand1 = ans;
+  ans = 0;
   displayElement.innerHTML = displayValue;
 }
+function operate(operation) {
+  switch (operation) {
+    case "add":
+      return operand1 + operand2;
 
+      break;
+    case "mult":
+      return operand1 * operand2;
+      break;
+    case "divide":
+      if (operand2 != 0) {
+        return (operand1 / operand2).toPrecision(2);
+      } else return "-1";
+      break;
+    case "minus":
+      return operand1 - operand2;
+      break;
+  }
+  console.log(operand1);
+}
 function numberClicked(buttonClass) {
   if (buttonClass[1] == "number") displayValue += buttonClass[0];
   innerDisplayValue = Number(displayValue);
-  displayValue = String(innerDisplayValue);
+  displayValue = String(innerDisplayValue); // used to get rid of the leading zero 
   displayElement.innerHTML = displayValue;
 }
